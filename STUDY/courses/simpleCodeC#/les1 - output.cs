272aed5409af92ca2f8b7f626facbbf751091a56
Den-One <76240293+Den-One@users.noreply.github.com>
2021-08-14 00:28:29 +0300

    Create les1 - output.cs

---
+++ STUDY/courses/simpleCodeC#/les1 - output.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			      Console.WriteLine("Hello World!");
+        }
+    }
+}
